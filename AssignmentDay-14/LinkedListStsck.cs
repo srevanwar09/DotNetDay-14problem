ef9b2a7bb495a4684dee42c9f9f0be291e537d57
srevanwar09 <srevanwar09@domain.com>
2022-09-11 21:27:08 +0530

    Adding PickPopStack program

---
+++ AssignmentDay-14/LinkedListStsck.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueueProblem
+{
+    internal class LinkedListStack
+    {
+        private Node top;
+        public LinkedListStack()
+        {
+            this.top = null;
+        }
+        internal void Push(int value) // this method is used the push or add the element in stack
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+                node.next = null;
+            else
+                node.next = this.top;
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack ", value);
+        }
+        public void Display() // this method is used to display the elements in stack
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+        internal void Peek() // this method is used to display the value which is at top
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine("\n{0} is in the top of the stack ", this.top.data);//NullrefrenceRxception
+        }
+        internal void Pop() // this method is used to pop or remove the last element in stack
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty, Deletion is not possible");
+                return;
+            }
+            Console.WriteLine("Value popped is {0} ", this.top.data);
+            this.top = this.top.next;
+        }
+        internal void isEmpty() // this method is used to peek and pop the elements till it is empty
+        {
+            while (top != null)
+            {
+                Peek();
+                Pop();
+            }
+            Console.WriteLine("Stack is Empty");
+        }
+    }
+}
