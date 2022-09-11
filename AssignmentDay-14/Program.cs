d9be38b06ff77597893efafad537382f635bb0db
srevanwar09 <srevanwar09@domain.com>
2022-09-11 15:13:18 +0530

    program are added

---
+++ AssignmentDay-14/Program.cs
@@ -0,0 +1,14 @@
+﻿using StackAndQueueProblem;
+
+LinkedListQueue linkedListQueue = new LinkedListQueue();
+linkedListQueue.Enqueue(56);
+linkedListQueue.Enqueue(30);
+linkedListQueue.Enqueue(70);
+linkedListQueue.Display();
+linkedListQueue.Dequeue();
+//LinkedListStack linkedListStack = new LinkedListStack();
+//linkedListStack.Push(70);
+//linkedListStack.Push(30);
+//linkedListStack.Push(56);
+//linkedListStack.Display();
+//linkedListStack.isEmpty();
